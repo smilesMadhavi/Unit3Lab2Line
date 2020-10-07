6270cc3fa4825b0a30fc006d5c6b8b71f76356b6
smilesMadhavi <72471453+smilesMadhavi@users.noreply.github.com>
2020-10-07 11:27:23 -0500

    Add files via upload

---
+++ LineRunner.java
@@ -0,0 +1,16 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab02f
+{
+	public static void main( String[] args )
+   {
+		//add test cases		
+		
+	}
+}
